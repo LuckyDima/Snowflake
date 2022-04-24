2562fbae4a55da1654fb8a42dc83a586793aeb9d
Dmitriy Ivanov <42345422+LuckyDima@users.noreply.github.com>
2022-04-24 15:14:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit
 import pandas
 
-streamlit.title('My Parents Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smothie')
-streamlit.text('Hard-Bolled Free-Range Egg')
-
-streamlit.title('My Mom\'s Healthy Diner')
+#streamlit.title('My Parents Healthy Diner')
+#streamlit.header('Breakfast Menu')
+#streamlit.text('Omega 3 & Blueberry Oatmeal')
+#streamlit.text('Kale, Spinach & Rocket Smothie')
+#streamlit.text('Hard-Bolled Free-Range Egg')
+#
+#streamlit.title('My Mom\'s Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
