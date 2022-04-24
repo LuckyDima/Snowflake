abb0a50e3942999a70b444b27454e17471fddc7b
Dmitriy Ivanov <42345422+LuckyDima@users.noreply.github.com>
2022-04-24 14:49:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smothie')
 streamlit.text('Hard-Bolled Free-Range Egg')
 
+streamlit.title('My Mom\'s Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
