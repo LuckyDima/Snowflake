f48bc666c5addb5dac5f74b47f8f70757784297f
Dmitriy Ivanov <42345422+LuckyDima@users.noreply.github.com>
2022-04-24 14:47:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents Healthy Diner')
-streamlit.title('My Mom\'s Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smothie')
+streamlit.text('Hard-Bolled Free-Range Egg')
